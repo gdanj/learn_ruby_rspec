098e364ea21d44bf6df7d02c77071a429d25a29b
dantho <dantho972@gmail.com>
2018-10-08 23:35:57 +0200

    ajout des dernier exo

--- 05_timer/timer.rb
+++ 05_timer/timer.rb
@@ -1,1 +1,4 @@
 #write your code here
+def time_string(x)
+  [x / 3600, x / 60 % 60, x % 60].map{|t| t.to_s.rjust(2,'0')}.join(':')
+end
--- 06_pig_latin/pig_latin.rb
+++ 06_pig_latin/pig_latin.rb
@@ -1,1 +1,22 @@
 #write your code here
+def translate(s)
+	tvoy = ["a","e","y","u","i","o"]
+	tab = s.split('')
+	i = tab.length
+	j = 0
+	k = 0
+	while tvoy.include?(v)
+		j+=1
+		
+		break
+
+		
+	end
+	if j == 0
+		k = tab[0]
+		tab[0] = tab[i]
+		tab[i] = k
+	end
+	tab += "ay".split('')
+	return tab.join('')
+end
