9324115566280941e6cb1000f3e6698ef6afd4ef
dantho <dantho972@gmail.com>
2018-10-09 01:07:05 +0200

    ajout des dernier test

--- 06_pig_latin/pig_latin.rb
+++ 06_pig_latin/pig_latin.rb
@@ -1,22 +1,31 @@
 #write your code here
 def translate(s)
-	tvoy = ["a","e","y","u","i","o"]
-	tab = s.split('')
-	i = tab.length
-	j = 0
-	k = 0
-	while tvoy.include?(v)
-		j+=1
-		
-		break
-
-		
+	tvoy = ["a","e","y","i","o"]
+	tdt = s.split(' ')
+	res = []
+	tdt.each do |v|
+		tab = v.split('')
+		i = tab.length
+		k = 0
+		j = 0
+		if tvoy.include?(tab[0])
+			j = 0
+		elsif tvoy.include?(tab[1])
+			j = 1
+		elsif tvoy.include?(tab[2])
+			j = 2
+		elsif tvoy.include?(tab[3])
+			j = 3
+		end
+		while j > 0
+			k = tab[0]
+			tab.delete_at(0)
+			tab[i] = k
+			j -= 1
+		end
+		tab += "ay".split('')
+		res << ' '
+		res += tab
 	end
-	if j == 0
-		k = tab[0]
-		tab[0] = tab[i]
-		tab[i] = k
-	end
-	tab += "ay".split('')
-	return tab.join('')
+	return res.join('').lstrip
 end
